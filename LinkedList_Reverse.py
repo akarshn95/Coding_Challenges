c611d707991be6fb04c6f6ed1501981418209fa2
Akarsh Nagaraj <akarshn95@gmail.com>
2020-07-22 19:36:45 -0400

    Create LinkedList_Reverse.py

---
+++ LinkedList_Reverse.py
@@ -0,0 +1,43 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        # use temp to swap node nexts an prev to store previous value
+        node = head
+        prev=None
+        temp=None
+        # store in temp to traverse the LL
+        while node:
+            temp=node.next
+            node.next=prev
+            prev=node
+            node=temp
+        head=prev
+        
+        return head
+    
+     def reverseList_recursive(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        
+        if not head or not head.next:
+            return head
+        
+        new_node = ListNode()
+        new_node = self.reverseList(head.next)
+        
+        head.next.next = head
+        head.next = None
+        return new_node
+        
+
+    
